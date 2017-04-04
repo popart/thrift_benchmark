f4f8e414a664188e61029d96fa8627399a66c5e9
Andrew Wong <andrew.wong@pelotoncycle.com>
2017-04-04 13:04:41 -0400

    client_multiprocess

---
+++ thrift_benchmark/client_multiprocess.py
@@ -0,0 +1,55 @@
+import sys
+sys.path.append('gen-py')
+
+from multiprocessing import Pool
+
+from echo import Echo
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+import timeit
+
+def worker(n):
+    # Make socket
+    transport = TSocket.TSocket('localhost', 9090)
+
+    # Buffering is critical. Raw sockets are very slow
+    transport = TTransport.TBufferedTransport(transport)
+
+    # Wrap in a protocol
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+    # Create a client to use the protocol encoder
+    client = Echo.Client(protocol)
+
+    # Connect!
+    transport.open()
+
+    client.reset()
+
+    for _ in xrange(n):
+        client.noop()
+
+    cs = client.count()
+
+    transport.close()
+    return max(cs.values())
+
+if __name__ == '__main__':
+
+    for n in xrange(1,4):
+        pool = Pool(processes=n)
+        ress = []
+        for i in xrange(n):
+            ress.append(pool.apply_async(worker, (100000,)))
+
+        pool.close()
+        pool.join()
+
+        print "clients: %d" % n
+        for res in ress:
+            print res.get()
+
